14ff0b293d5699ab7559595e8f3520aad771c37e
Michal Kreft <michal.kreft@softwire.com>
2020-06-27 13:41:06 +0100

    Handle null values

--- src/main/kotlin/com/gitTraining/Fibbonaci.kt
+++ src/main/kotlin/com/gitTraining/Fibbonaci.kt
@@ -1,5 +1,10 @@
 package com.gitTraining
 
+fun computeFibbonaciNumber(position: Int?): Int {
+    var notNullPosition = position
+    if (notNullPosition == null) {
+        notNullPosition = 1
+    }
 fun computeFibbonaciNumber(position: Int): Int {
     if (position == 0) return 0
     if (position < 0) {
@@ -8,12 +13,17 @@ fun computeFibbonaciNumber(position: Int): Int {
     var i = 1
     var j = 1
 
+    if (notNullPosition <= 2) return 1
     if (position == 1 || position == 2) return 1
 
     var smallFibbonachiNumber = 1
     var largeFibbonachiNumber = 1
 
     var currentPosition = 2
+    while (currentPosition < notNullPosition) {
+        val temp = i
+        i = j
+        j += temp
     while (currentPosition < position) {
         val nextFibbonachiNumber = smallFibbonachiNumber + largeFibbonachiNumber
         smallFibbonachiNumber = largeFibbonachiNumber
